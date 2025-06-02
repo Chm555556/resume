3507c3bfb1af1ad356c3db105c6bfec072509e51
Chandra Mohan <chandramohanmahto0170@gmail.com>
2025-06-02 16:40:52 +0530

    Initial commit - cleaned repo

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <iframe
+    // src="/resume.pdf"
+    src="Chandra_Mohan_Mahto_Resume.pdf"
+    style={{
+        width: "100%",
+        height: "100vh",
+        border: "none"
+      }}
+    ></iframe>
+  );
+}
